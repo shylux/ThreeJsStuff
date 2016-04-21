9774f5030addd3390dd58c1b1384f49b50fe8a3c
Shylux <shylux@gmail.com>
2016-04-22 00:04:38 +0200

    Basic scene.

--- script.js
+++ script.js
@@ -34,27 +34,41 @@ function init() {
     );
     geometry.computeBoundingSphere()
 
-    var material = new THREE.MeshBasicMaterial({
-        color: 0x000000,
-        wireframe: true,
+    var material = new THREE.MeshPhongMaterial({
+        color: 0xff0000
     });
     mesh = new THREE.Mesh(geometry, material);
     scene.add(mesh);
 
+    var geom_plane = new THREE.PlaneGeometry( 10, 10 );
+    var mat_plane = new THREE.MeshPhongMaterial( {color: 0xff00ff} );
+    var plane = new THREE.Mesh( geom_plane, mat_plane );
+    plane.rotation.x = - Math.PI / 2;
+    scene.add(plane);
+
+    // Lights
+    var light = new THREE.AmbientLight( 0x0000ff );
+    scene.add( light );
+
+    var directionalLight = new THREE.DirectionalLight( 0xffffff, 1 );
+    directionalLight.position.set( 1, 1, 0 );
+    scene.add(directionalLight);
+
+    // Camera
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 5;
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setClearColor( 0xffffff, 1);
     document.body.appendChild(renderer.domElement);
+
+    var orbit = new THREE.OrbitControls( camera, renderer.domElement )
 }
 
 function animate() {
     requestAnimationFrame(animate);
-    mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.01;
-    mesh.rotation.z += 0.01;
+    // mesh.rotation.x += 0.005;
+    mesh.rotation.y += 0.005;
     renderer.render(scene, camera);
 }
 animate();
